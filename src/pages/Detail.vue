bb6d687956043fc36e90fb519a45728cb6a45bf6
KimRuse <1327192007@qq.com>
2019-05-31 11:18:14 +0800

    add detail-header

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -1,17 +1,25 @@
 <template>
-  <Banner />
+  <div>
+    <Banner />
+    <Header />
+    <div class="content"></div>
+  </div>
 </template>
 
 <script>
 import Banner from './detail/components/Banner'
+import Header from './detail/components/Header'
 
 export default {
   name: 'Detail',
   components: {
-    Banner
+    Banner, Header
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.content {
+  height: 50rem;
+}
 </style>
